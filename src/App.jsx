5f387a401f1651093300ae25955f5cb9f67c5c38
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-04-14 11:52:11 -0700

    created heroSection

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@
 //import viteLogo from './assets/vite.svg'
 //import heroImg from './assets/hero.png'
 import './App.css'
-import Header from './components/header'
-import './components/Header.css'
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 
 function App() {
   
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   )
 }
